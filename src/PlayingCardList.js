17f463b65734d4f3232f72e3c35f4549c007a32e
nwog-jbonilla <jbonilla@neworg.com>
2024-10-09 18:47:05 -0400

    Step 3

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -1,19 +1,15 @@
-import React, { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
-import axios from "axios";
+import React from "react";
+import {useAxios} from "./hooks";
+
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
 
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function CardTable() {
-  const [cards, setCards] = useState([]);
-  const addCard = async () => {
-    const response = await axios.get(
-      "https://deckofcardsapi.com/api/deck/new/draw/"
-    );
-    setCards(cards => [...cards, { ...response.data, id: uuidv4() }]);
-  };
+    
+  const [cards, addCard] = useAxios("https://deckofcardsapi.com/api/deck/new/draw/");
+  
   return (
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,7 @@
-﻿import React, { useState } from 'react';
+﻿import { useState } from 'react';
+import axios from "axios";
+import {v4 as uuidv4} from "uuid";
+
 const useFlip = () => {
     const [state, setState] = useState(true);
     const flipState = () => {
@@ -6,4 +9,17 @@ const useFlip = () => {
     }
     return [state, flipState]
 }
-export default useFlip;
+
+const useAxios = (url) => {
+    
+    const [items, setItems] = useState([]);
+
+    const addItem = async () => {
+        const response = await axios.get(url);
+        setItems(items => [...items, { ...response.data, id: uuidv4() }]);
+    };
+
+    return [items, addItem]
+}
+    
+export { useFlip, useAxios };
